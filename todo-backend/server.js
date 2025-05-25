b6d19dec517eba2a1f5a13283888f503117f6116
GerCop <copaduceo@gmail.com>
2025-05-25 16:49:20 -0600

    Iniciando el proyecto

---
+++ todo-backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
