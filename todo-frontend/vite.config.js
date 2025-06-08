123fbd4d2d6eab77e8424c290373873083c8cdc9
GerCop <copaduceo@gmail.com>
2025-06-08 09:15:12 -0600

    Se queda el archivo solo para que pueda pasar la variable de entorno en vercel

--- todo-frontend/vite.config.js
+++ todo-frontend/vite.config.js
@@ -1,35 +1,17 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // Cargar variables de entorno
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [react()],
-    css: {
-      modules: {
-        localsConvention: 'camelCase'
-      }
-    },
-    resolve: {
-      alias: {
-        '@': '/src'
-      }
-    },
-    // Configuración del servidor de desarrollo
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL || 'http://localhost:4000', // Fallback para desarrollo
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
-      }
-    },
-    // Exponer variables al cliente
-    define: {
-      'process.env': env
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCase' // Permite usar styles.loginContainer
     }
-  };
+  },
+  resolve: {
+    alias: {
+      '@': '/src' // Para poder usar importaciones como '@/assets/...'
+    }
+  }
 });
